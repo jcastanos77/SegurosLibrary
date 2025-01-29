781b0f909c4f9393b2d94df34d8a30c4518e8bcb
jecastanosm <jecastanosm@concredito.com.mx>
2025-01-29 10:58:06 -0700

    cambios nice

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "SegurosLibrary",
             targets: ["SegurosLibrary"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/jcastanos77/SegurosLibrary.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
---
+++ Sources/SegurosLibrary/PruebitaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PruebitaViewController.swift
+//  SegurosLibrary
+//
+//  Created by JECASTAÑOSM on 29/01/25.
+//
+
+import UIKit
+
+public class PruebitaViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
