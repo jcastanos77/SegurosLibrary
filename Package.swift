89a3198dc555f8341fabd15aaaac2edf9d396a68
jecastanosm <jecastanosm@concredito.com.mx>
2025-02-10 13:08:12 -0700

    cambios

--- Package.swift
+++ Package.swift
@@ -5,17 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "SegurosLibrary",
-    platforms: [
-        .iOS(.v15)
-    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SegurosLibrary",
             targets: ["SegurosLibrary"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/jcastanos77/SegurosLibrary.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
